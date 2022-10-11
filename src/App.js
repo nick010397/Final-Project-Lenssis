703e861aa3f62b6b271c1af3d2a0b3c0dd4de644
jyun9807 <jyun9807@gmail.com>
2022-10-11 14:37:01 +0900

    style:서로 공유하지 않는 것 삭제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Notice from './components/main/Notice';
 import Main from "./pages/Main"
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/notice" element={<Notice/>} />
       <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
     </Routes>
